b6dfb68d98769be0152d64e3481e319468fd354d
Jensen <rentenglong@163.com>
2020-11-05 00:18:20 +0800

    添加UI层操作控制按钮 添加大量按钮美术资源

--- Build/Android/src/com/YourCompany/PlateShootingGame/OBBData.java
+++ Build/Android/src/com/YourCompany/PlateShootingGame/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-71283690L // the length of the file in bytes
+82577644L // the length of the file in bytes
 )
 };
 };
